f7516f76226508f715b546532eefeb02cf8f0bc8
jordiBBigle <jordi.bosch@biglelegal.com>
2022-02-13 14:09:27 +0100

    Add absolute rouing

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -9,7 +9,7 @@ import { NavbarTap } from 'src/app/entities/routingTap';
 export class HeaderComponent {
 
   public headerOptions: Array<NavbarTap> = [
-    new NavbarTap('Hypothesis testing', 'hypothesis-testing/testList'),
+    new NavbarTap('Hypothesis testing', '/hypothesis-testing/testList'),
     new NavbarTap('Confidence Interval', '/confidence-interval'),
     new NavbarTap('Regression coefficients', '/regression-coefficients'),
     new NavbarTap('Distributions graphs', '/distributions')
