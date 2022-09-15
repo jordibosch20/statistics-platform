fcb8a90338358378e283043775c0e0e08be46c17
Jordi Bosch <jordiboschbosch@hotmail.com>
2022-09-15 14:28:49 +0200

    Correclty configure calculators

--- src/app/descriptive-statistics/descriptive-statistics.routing.module.ts
+++ src/app/descriptive-statistics/descriptive-statistics.routing.module.ts
@@ -2,6 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DescriptiveStatisticsComponent } from 'src/app/descriptive-statistics/descriptive-statistics.component';
 import { DescriptiveStatisticsListComponent } from 'src/app/descriptive-statistics/pages/descriptive-statistics-list/descriptive-statistics-list.component';
+import { GeometricMeanComponent } from 'src/app/descriptive-statistics/pages/geometric-mean/geometric-mean.component';
+import { HarmonicMeanComponent } from 'src/app/descriptive-statistics/pages/harmonic-mean/harmonic-mean.component';
+import { KurtosisComponent } from 'src/app/descriptive-statistics/pages/kurtosis/kurtosis.component';
+import { MeanComponent } from 'src/app/descriptive-statistics/pages/mean/mean.component';
+import { SkewnessComponent } from 'src/app/descriptive-statistics/pages/skewness/skewness.component';
+import { SumCubesComponent } from 'src/app/descriptive-statistics/pages/sum-cubes/sum-cubes.component';
+import { SumInversesComponent } from 'src/app/descriptive-statistics/pages/sum-inverses/sum-inverses.component';
+import { SumSquaresComponent } from 'src/app/descriptive-statistics/pages/sum-squares/sum-squares.component';
 import { VarianceComponent } from 'src/app/descriptive-statistics/pages/variance/variance.component';
 
 
@@ -19,35 +27,35 @@ const routes: Routes = [
       },
       {
         path: 'mean-median-mode',
-        component: VarianceComponent
+        component: MeanComponent
       },
       {
         path: 'skewness',
-        component: VarianceComponent
+        component: SkewnessComponent
       },
       {
         path: 'kurtosis',
-        component: VarianceComponent
+        component: KurtosisComponent
       },
       {
         path: 'geometric-mean',
-        component: VarianceComponent
+        component: GeometricMeanComponent
       },
       {
         path: 'harmonic-mean',
-        component: VarianceComponent
+        component: HarmonicMeanComponent
       },
       {
         path: 'sum-squares',
-        component: VarianceComponent
+        component: SumSquaresComponent
       },
       {
         path: 'sum-cubes',
-        component: VarianceComponent
+        component: SumCubesComponent
       },
       {
         path: 'sum-inverses',
-        component: VarianceComponent
+        component: SumInversesComponent
       },
     ]
   }
